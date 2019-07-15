e632dfd2b9c8fbe694a677583462cd71f7af6c4c
Chen Zhe <chen.zhe@dsanalytics.com>
2019-07-15 16:58:32 +0800

    exception handling

---
+++ query_backend/backend_factory.py
@@ -0,0 +1,11 @@
+from query_backend.sql.google_bigquery import BigQueryBackend
+from query_backend.sql.sqlite import Sqlite3Backend
+from errors import ServerException
+
+def get_sql_backend(name, *args):
+    if name == "bigquery":
+        return BigQueryBackend(*args)
+    elif name == "sqlite":
+        return Sqlite3Backend(*args)
+    else:
+        raise ServerException(f"Unknown backend configured: '{name}'")
--- query_backend/query_functions.py
+++ query_backend/query_functions.py
@@ -1,12 +1,14 @@
-from query_backend.sql.sqlite import Sqlite3Backend
+from query_backend.backend_factory import get_sql_backend
 
-def test():
-    backend = Sqlite3Backend()
+backend = "sqlite"
+
+def total_trips_over_date_range(start_date, end_date):
+    sql_backend = get_sql_backend(backend)
     query = (
         "SELECT date(dropoff_datetime) AS date, count(*) AS total_trips FROM `tlc_yellow_trips_2016` "
         "WHERE date(dropoff_datetime) <=  '2016-01-06' AND date(dropoff_datetime) >=  '2016-01-01'"
         "GROUP BY date(dropoff_datetime)")
-    result = backend.query(query).response
+    result = sql_backend.query(query).response
     return [{key: value for (key, value) in zip(result.columns, row)}
             for row in result.values]
 
