8f5b00d7c55b37794284480d32c4a061746fbbd8
Chen Zhe <chen.zhe@dsanalytics.com>
2019-07-16 14:27:45 +0800

    Add logging bug fix
    SQL backend has to be created when the function is executed

--- errors.py
+++ errors.py
@@ -1,7 +1,9 @@
 from functools import wraps
 import json
 from query.query_commons import HttpResponse
+import logging
 
+logger = logging.getLogger(__name__)
 
 class RequestException(Exception):
 	pass
@@ -20,11 +22,13 @@ def handle_exceptions(f):
             return HttpResponse(json.dumps(error), HttpResponse.ContentType.JSON, HttpResponse.Status.BAD_REQUEST)
         
         except ServerException as e:
-            error = {"status": "ServerError", "reason": str(e)}
+            logger.exception("Server Exception Occurred")
+            error = {"status": "ServerError", "reason": ""}
             return HttpResponse(json.dumps(error), HttpResponse.ContentType.JSON, HttpResponse.Status.INTERNAL_SERVER_ERROR)
 
         except Exception as e:
-            error = {"status": "UnknownError", "reason": str(e)}
-            return HttpResponse(json.dumps(error), HttpResponse.ContentType.JSON, HttpResponse.Status.UNKNOWN_ERROR)
+            logger.exception("Internal Server Error Occurred")
+            error = {"status": "ServerError", "reason": ""}
+            return HttpResponse(json.dumps(error), HttpResponse.ContentType.JSON, HttpResponse.Status.INTERNAL_SERVER_ERROR)
 
     return decorated
