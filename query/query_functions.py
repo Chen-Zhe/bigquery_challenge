fb52ddbfffd5d5410b8b35e4e075fae61e2ed890
Chen Zhe <chenzhesg@gmail.com>
2019-07-18 20:41:57 +0800

    correct table name error

--- query/query_functions.py
+++ query/query_functions.py
@@ -21,11 +21,11 @@ else:
                                                        "2016-01-01", "2016-12-31"))
     tables.register_table("tlc_green_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2017",
                                                        "2017-01-01", "2017-12-31"))
-    tables.register_table("tlc_yellow_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2015",
+    tables.register_table("tlc_yellow_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2015",
                                                         "2015-01-01", "2015-12-31"))
-    tables.register_table("tlc_yellow_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2016",
+    tables.register_table("tlc_yellow_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2016",
                                                         "2016-01-01", "2016-12-31"))
-    tables.register_table("tlc_yellow_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_green_trips_2017",
+    tables.register_table("tlc_yellow_trips", DataTable("bigquery-public-data.new_york_taxi_trips.tlc_yellow_trips_2017",
                                                         "2017-01-01", "2017-12-31"))
 
 
